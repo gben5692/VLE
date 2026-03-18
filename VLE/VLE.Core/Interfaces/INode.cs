c35335fa727337126c40fee46fa4fcdc1011567e
gben5692 <gben5692@gmail.com>
2026-03-18 17:14:29 +0100

    Add project files.

---
+++ VLE/VLE.Core/Interfaces/INode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VLE.VLE.Core.Interfaces
+{
+    public interface INode
+    {
+
+
+
+    }
+}
