c5044d018e1d810e48291053d7cdc285a02250d8
gben5692 <gben5692@gmail.com>
2026-03-20 22:58:07 +0100

    Introduced the wire class for handling IPin logic for future "Connection"

---
+++ VLE/VLE.Core/Handler/Wire.cs
@@ -0,0 +1,33 @@
+﻿using VLE.VLE.Core.Interfaces;
+
+namespace VLE.VLE.Core.Handler
+{
+    public class Wire
+    {
+        public IPin? StartPoint { get; set; }
+        public List<IPin>? EndPoints { get; set; }
+
+
+        public Wire(IPin startPoint, List<IPin> endPoints) 
+        {
+            if (startPoint == null)  // if the enpoint and startpoint is null then you cant draw the wire
+            {
+                throw new ArgumentNullException(nameof(startPoint));
+            }
+            else if (endPoints == null)
+            {
+                throw new ArgumentNullException(nameof(endPoints));
+            }
+            else
+            {
+                if (endPoints.All(endPoint => endPoint.DataType != startPoint.DataType)) // check if the startpoint and endpoint is not the same type 
+                {
+                    throw new InvalidOperationException("Startpoint and Endpoint is not the same type");
+                }
+
+                StartPoint = startPoint;
+                EndPoints = endPoints;
+            }
+        }
+    }
+}
